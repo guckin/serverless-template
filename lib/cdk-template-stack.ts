e82d7f765793699525cd2ec6e4dbfc96b6a525ec
guckin <donald.stephen.mcguckin@gmail.com>
2022-09-03 15:28:38 -0700

    init-commit

--- lib/cdk-template-stack.ts
+++ lib/cdk-template-stack.ts
@@ -1,16 +1,27 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {Pass, StateMachine, StateMachineType} from '@aws-cdk/aws-stepfunctions';
+import {RestApi, StepFunctionsIntegration, StepFunctionsRestApi} from '@aws-cdk/aws-apigateway';
 
 export class CdkTemplateStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkTemplateQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        const pass = new Pass(this, 'PassState', {
+            result: {value:"Hello!"},
+        })
+
+        const stateMachine = new StateMachine(this, 'MyStateMachine', {
+            definition: pass,
+            stateMachineType: StateMachineType.STANDARD,
+        });
+
+        const api = new RestApi(this, 'StepFunctionsRestApi', {
+            restApiName: 'StateMachineAPI',
+            deploy: true
+        });
+
+        api.root.addMethod('POST', StepFunctionsIntegration.startExecution(stateMachine));
+    }
 }
