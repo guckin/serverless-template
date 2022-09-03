bc3ae0fdeb7a76dccea1e40c729446e17ece8a8b
guckin <donald.stephen.mcguckin@gmail.com>
2022-09-03 16:56:04 -0700

    fixes context issues with stack scoping

--- lib/cdk-template-stack.ts
+++ lib/cdk-template-stack.ts
@@ -8,16 +8,16 @@ export class CdkTemplateStack extends Stack {
         super(scope, id, props);
 
 
-        const pass = new Pass(scope, 'PassState', {
+        const pass = new Pass(this, 'PassState', {
             result: {value:"Hello!"},
         })
 
-        const stateMachine = new StateMachine(scope, 'MyStateMachine', {
+        const stateMachine = new StateMachine(this, 'MyStateMachine', {
             definition: pass,
             stateMachineType: StateMachineType.STANDARD,
         });
 
-        const api = new RestApi(scope, 'StepFunctionsRestApi', {
+        const api = new RestApi(this, 'StepFunctionsRestApi', {
             restApiName: 'StateMachineAPI',
             deploy: true
         });
