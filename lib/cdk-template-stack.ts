0c8ed1c24abff980544bcc02f63f19f8d3920310
guckin <donald.stephen.mcguckin@gmail.com>
2022-09-03 16:51:37 -0700

    removes reference to cdk v1

--- lib/cdk-template-stack.ts
+++ lib/cdk-template-stack.ts
@@ -1,23 +1,23 @@
-import * as cdk from 'aws-cdk-lib';
+import {Stack, StackProps} from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import {Pass, StateMachine, StateMachineType} from '@aws-cdk/aws-stepfunctions';
-import {RestApi, StepFunctionsIntegration, StepFunctionsRestApi} from '@aws-cdk/aws-apigateway';
+import {Pass, StateMachine, StateMachineType} from 'aws-cdk-lib/aws-stepfunctions';
+import {RestApi, StepFunctionsIntegration} from 'aws-cdk-lib/aws-apigateway';
 
-export class CdkTemplateStack extends cdk.Stack {
-    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class CdkTemplateStack extends Stack {
+    constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
 
 
-        const pass = new Pass(this, 'PassState', {
+        const pass = new Pass(scope, 'PassState', {
             result: {value:"Hello!"},
         })
 
-        const stateMachine = new StateMachine(this, 'MyStateMachine', {
+        const stateMachine = new StateMachine(scope, 'MyStateMachine', {
             definition: pass,
             stateMachineType: StateMachineType.STANDARD,
         });
 
-        const api = new RestApi(this, 'StepFunctionsRestApi', {
+        const api = new RestApi(scope, 'StepFunctionsRestApi', {
             restApiName: 'StateMachineAPI',
             deploy: true
         });
