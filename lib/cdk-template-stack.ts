b273fe95994b42a2f476d45453464b045f2ee747
guckin <donald.stephen.mcguckin@gmail.com>
2022-09-04 00:46:13 -0700

    adds endpoint for `POST /dogs` backed by dynamo db and request models

--- lib/cdk-template-stack.ts
+++ lib/cdk-template-stack.ts
@@ -1,16 +1,35 @@
 import {Stack, StackProps} from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import {Pass, StateMachine, StateMachineType} from 'aws-cdk-lib/aws-stepfunctions';
-import {RestApi, StepFunctionsIntegration} from 'aws-cdk-lib/aws-apigateway';
+import {Construct} from 'constructs';
+import {JsonPath, StateMachine, StateMachineType} from 'aws-cdk-lib/aws-stepfunctions';
+import {JsonSchemaType, Model, RestApi, StepFunctionsIntegration} from 'aws-cdk-lib/aws-apigateway';
+import {AttributeType, Table} from 'aws-cdk-lib/aws-dynamodb';
+import {DynamoAttributeValue, DynamoPutItem} from 'aws-cdk-lib/aws-stepfunctions-tasks';
 
 export class CdkTemplateStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
 
+        const dynamoTable = new Table(this, 'DogTable', {
+            partitionKey: {
+                name: 'id',
+                type: AttributeType.STRING
+            }
+        });
 
-        const pass = new Pass(this, 'PassState', {
-            result: {value:"Hello!"},
-        })
+        dynamoTable.autoScaleWriteCapacity({
+            minCapacity: 1,
+            maxCapacity: 10
+        });
+
+        const pass = new DynamoPutItem(this, 'PutItem', {
+            item: {
+                id: DynamoAttributeValue.fromString(JsonPath.stringAt('States.UUID()')),
+                name: DynamoAttributeValue.fromString(JsonPath.stringAt('$.name')),
+                breed: DynamoAttributeValue.fromString(JsonPath.stringAt('$.breed'))
+            },
+            table: dynamoTable,
+            resultPath: '$.Item',
+        });
 
         const stateMachine = new StateMachine(this, 'MyStateMachine', {
             definition: pass,
@@ -22,6 +41,35 @@ export class CdkTemplateStack extends Stack {
             deploy: true
         });
 
-        api.root.addMethod('POST', StepFunctionsIntegration.startExecution(stateMachine));
+        const dogModel: Model = api.addModel(
+            'DogModel',
+            {
+                schema: {
+                    type: JsonSchemaType.OBJECT,
+                    properties: {
+                        name: {
+                            type: JsonSchemaType.STRING
+                        },
+                        breed: {
+                            type: JsonSchemaType.STRING
+                        }
+                    },
+                    required: ['name', 'breed']
+                }
+            }
+        );
+
+        api.root.addResource('/dogs').addMethod(
+            'POST',
+            StepFunctionsIntegration.startExecution(stateMachine),
+            {
+                requestModels: {
+                    'application/json': dogModel
+                },
+                requestValidatorOptions: {
+                    validateRequestBody: true
+                }
+            }
+        );
     }
 }
