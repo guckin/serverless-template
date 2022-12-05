d3635d9926d3e207d96c899e5db407f522b54e77
Stephen McGuckin <stephen.mcguckin@nordicsemi.no>
2022-12-04 22:51:41 -0800

    moved build output to dist folder

--- bin/create-hello-world.ts
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { HelloWorldServiceStack } from '../lib/hello-world-stack';
-
-const app = new cdk.App();
-new HelloWorldServiceStack(app, 'HelloWorld', {
-    env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-});
--- lib/container.ts
+++
@@ -1,8 +0,0 @@
-import { HelloworldHandler } from "./hello-world-handler";
-import { ConsoleLogger } from "./logger";
-
-const logger = new ConsoleLogger();
-
-export const helloWorldHandler = new HelloworldHandler(logger).invoke;
-export const hellowWorldPath = __filename;
-export const helloWorldHandlerName = 'helloWorldHandler';
