2fce40be6fe1799103bccecc6b5782b58205b1f7
Stephen McGuckin <stephen.mcguckin@nordicsemi.no>
2023-02-28 00:29:07 -0800

    updates the env var for the stage to match what is actually being passed by the deployment

--- src/create-stack.ts
+++ src/create-stack.ts
@@ -4,7 +4,7 @@ import * as cdk from 'aws-cdk-lib';
 import { HelloWorldServiceStack } from './hello-world-stack';
 
 const app = new cdk.App();
-const stage = process.env.Stage || 'dev';
+const stage = process.env.STAGE || 'dev';
 new HelloWorldServiceStack(app, `HelloWorld-${stage}`, {
     env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
     stage
