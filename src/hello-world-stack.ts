a85a4b9212e65434d1deb7f82ba638f7ef61e4a8
Stephen McGuckin <stephen.mcguckin@nordicsemi.no>
2023-02-28 21:44:48 -0800

    removes esbuild as a dependency
    The NodeJs construct use esbuild under the hood.

--- src/hello-world-stack.ts
+++ src/hello-world-stack.ts
@@ -1,8 +1,7 @@
 import {Stack, StackProps} from 'aws-cdk-lib';
 import * as path from 'path';
 import {Construct} from 'constructs';
-import {NodejsFunction} from 'aws-cdk-lib/aws-lambda-nodejs';
-import {Runtime} from 'aws-cdk-lib/aws-lambda';
+import {Code, Runtime, Function} from 'aws-cdk-lib/aws-lambda';
 import {BasePathMapping, Cors, DomainName, EndpointType, LambdaIntegration, RestApi} from 'aws-cdk-lib/aws-apigateway';
 import {Certificate} from 'aws-cdk-lib/aws-certificatemanager';
 
@@ -12,44 +11,41 @@ export type HelloWorldProps = StackProps & {
 
 export class HelloWorldServiceStack extends Stack {
     constructor(scope: Construct, id: string, props: HelloWorldProps) {
-            super(scope, id, props);
+        super(scope, id, props);
 
-            const apiFunction = new NodejsFunction(this, 'ApiFunction', {
-                entry: path.join(__dirname, '..', 'build', 'hello-world-handler.js'),
-                handler: 'handler',
-                runtime: Runtime.NODEJS_16_X,
-                environment: {
-                    NODE_OPTIONS: '--enable-source-maps',
-                }
-            });
+        const apiFunction = new Function(this, 'MyFunction', {
+          runtime: Runtime.NODEJS_16_X,
+          handler: 'hello-world-handler.handler',
+          code: Code.fromAsset(path.join(__dirname, '..', 'build'))
+        });
 
-            const lambdaIntegration = new LambdaIntegration(apiFunction);
+        const lambdaIntegration = new LambdaIntegration(apiFunction);
 
-            const api = new RestApi(this, 'API', {
-                defaultCorsPreflightOptions: {
-                    allowOrigins: Cors.ALL_ORIGINS,
-                    allowMethods: Cors.ALL_METHODS
-                }
-            });
-            api.root.addMethod('GET', lambdaIntegration);
+        const api = new RestApi(this, 'API', {
+            defaultCorsPreflightOptions: {
+                allowOrigins: Cors.ALL_ORIGINS,
+                allowMethods: Cors.ALL_METHODS
+            }
+        });
+        api.root.addMethod('GET', lambdaIntegration);
 
-            const cert = Certificate.fromCertificateArn(
-              this,
-              'cert',
-              'arn:aws:acm:us-east-1:084882962555:certificate/729d47e9-8d3b-439c-b4b5-e74a9a33cbce'
-            );
+        const cert = Certificate.fromCertificateArn(
+          this,
+          'cert',
+          'arn:aws:acm:us-east-1:084882962555:certificate/729d47e9-8d3b-439c-b4b5-e74a9a33cbce'
+        );
 
-            const domainName = new DomainName(this, 'DomainName', {
-                domainName: `${props.stage}.api.helpfl.click`,
-                certificate: cert,
-                endpointType: EndpointType.EDGE,
-            });
+        const domainName = new DomainName(this, 'DomainName', {
+            domainName: `${props.stage}.api.helpfl.click`,
+            certificate: cert,
+            endpointType: EndpointType.EDGE,
+        });
 
-            new BasePathMapping(this, 'ApiMapping', {
-                domainName: domainName,
-                restApi: api,
-                basePath: 'hello-world'
-            });
+        new BasePathMapping(this, 'ApiMapping', {
+            domainName: domainName,
+            restApi: api,
+            basePath: 'hello-world'
+        });
 
     }
 }
